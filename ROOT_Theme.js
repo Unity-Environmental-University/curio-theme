b2207eb902193dd97b4716616990a37db9515df4
unity-hallie <157060474+unity-hallie@users.noreply.github.com>
2024-03-01 11:37:17 -0500

    Fixed a bug where link was not being rerun after tray opened again.

--- ROOT_Theme.js
+++ ROOT_Theme.js
@@ -11,26 +11,27 @@ $(document).ready(function (classNames){
 
     let userID = ENV['current_user_id'];
 
-
-    $('#global_nav_help_link').click(function(event){
-        console.log("Clicked");
-        let tryAddButton;
-        tryAddButton = () => {
-            console.log("Done Waiting");
-            let els = document.querySelectorAll('#nav-tray-portal [href^="https://unitycollege.tfaforms.net/"]');
-            if(els && els.length > 0) {
-                for(let el of els) {
-                    if(el.href.includes(userID)) continue;
-                    el.href += userID;
-                    console.log(el.href);
+    let el = document.getElementById('global_nav_help_link');
+    if(el) {
+        el.addEventListener('click', () => {
+            console.log("Clicked");
+            let tryAddButton;
+            tryAddButton = () => {
+                console.log("Done Waiting");
+                let els = document.querySelectorAll('#nav-tray-portal [href^="https://unitycollege.tfaforms.net/"]');
+                if (els && els.length > 0) {
+                    for (let el of els) {
+                        if (el.href.includes(userID)) continue;
+                        el.href += userID;
+                        console.log(el.href);
+                    }
+                } else {
+                    setTimeout(tryAddButton, 200);
                 }
-            } else {
-                setTimeout(tryAddButton, 200);
             }
-        }
-        tryAddButton();
-        return false;
-    });
+            tryAddButton();
+        })
+    }
 }); // end doc ready
 
 /////////////////////////////////////////////
