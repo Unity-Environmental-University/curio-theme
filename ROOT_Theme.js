47d0d87c9cc953aae23bd2fcae4b91316bb36312
unity-hallie <157060474+unity-hallie@users.noreply.github.com>
2024-03-14 09:34:23 -0400

    Factored fixLinksInNodes out its own function so it could be run at page load.

--- ROOT_Theme.js
+++ ROOT_Theme.js
@@ -4,14 +4,21 @@
 ////////////// AS OF 2-20-2024 //////////////
 /////////////////////////////////////////////
 
+function fixLinksInNodes(parentNode, userID) {
+    let els = parentNode.querySelectorAll('[href^="https://unitycollege.tfaforms.net/"]')
+    if (els && els.length > 0) {
+        for (let el of els) {
+            if (el.href.includes(userID)) continue;
+            el.href += userID;
+        }
+    }
 
+}
 
 $(document).ready(function (classNames){
-
     console.log('ElevateActual custom javascript is running');
-
     let userID = ENV['current_user_id'];
-
+    fixLinksInNodes(document, userID);
     let el = document.getElementById('global_nav_help_link');
     if(el) {
         el.addEventListener('click', () => {
@@ -36,14 +43,8 @@ $(document).ready(function (classNames){
 
     let observer = new MutationObserver((mutations)=>{
         for (let mutation of mutations) {
+            fixLinksInNodes(mutation.target, userID);
             console.log(mutation.target);
-            let els = mutation.target.querySelectorAll('[href^="https://unitycollege.tfaforms.net/"]')
-            if (els && els.length > 0) {
-                for (let el of els) {
-                    if (el.href.includes(userID)) continue;
-                    el.href += userID;
-                }
-            }
         }
     });
 
